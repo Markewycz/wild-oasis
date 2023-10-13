7849a8712e63d546bb3e344d73f26e802957835f
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-10-13 18:11:42 +0200

    feat: supabase connection

---
+++ src/services/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://sbociipmstesqvrjraxr.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNib2NpaXBtc3Rlc3F2cmpyYXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4ODAzNjEsImV4cCI6MjAxMjQ1NjM2MX0.aj0HYcH7MYvxnhorwwrrusAMYTohLoc6d3BX_ToJjik';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
