4e04d484c876c80f92a8ec12469ea5ad64bc860a
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-10-30 17:57:43 +0100

    feat: header with user name, avatar, logout and settings

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('example@example.com');
-  const [password, setPassword] = useState('1234');
+  const [email, setEmail] = useState('kontakt.gmarkiewicz@gmail.com');
+  const [password, setPassword] = useState('12345678');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
